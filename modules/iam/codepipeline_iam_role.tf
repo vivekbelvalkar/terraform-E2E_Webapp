c60199b5443b952470cd764031a01c5b3eff171a
vivekbelvalkar <vvkbel@gmail.com>
2026-01-14 15:11:47 +0000

    fix "unknown policy" issue

--- modules/iam/codepipeline_iam_role.tf
+++ modules/iam/codepipeline_iam_role.tf
@@ -15,7 +15,40 @@ resource "aws_iam_role" "codepipeline" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "codepipeline_full" {
-  role       = aws_iam_role.codepipeline.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+# resource "aws_iam_role_policy_attachment" "codepipeline_full" {
+#   role       = aws_iam_role.codepipeline.name
+#   policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+# } -----> Did not work , hence inline policy (this one can be used instead "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess")
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "dev-ems-codepipeline-policy"
+  role = aws_iam_role.codepipeline.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:PutObject"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuilds"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = "iam:PassRole"
+        Resource = "*"
+      }
+    ]
+  })
 }
