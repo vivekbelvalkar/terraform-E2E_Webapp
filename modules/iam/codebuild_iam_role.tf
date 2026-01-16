03c9f92f928eca245f1ed6f83599c3ea378e7b73
vivekbelvalkar <vvkbel@gmail.com>
2026-01-16 10:52:47 +0000

    fix role policy syntax

--- modules/iam/codebuild_iam_role.tf
+++ modules/iam/codebuild_iam_role.tf
@@ -3,28 +3,41 @@ resource "aws_iam_role" "codebuild" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "codebuild.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    },
-    
-    {
-      Effect = "Allow",
-      Action = [
-      "autoscaling:StartInstanceRefresh",
-      "autoscaling:DescribeAutoScalingGroups"
-      ],
-      Resource = "*"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 
   tags = {
-    name= "${var.env}-ems-codebuild-role"
+    name = "${var.env}-ems-codebuild-role"
   }
 }
 
 resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   role       = aws_iam_role.codebuild.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+resource "aws_iam_role_policy" "codebuild_asg_policy" {
+  role = aws_iam_role.codebuild.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:StartInstanceRefresh",
+          "autoscaling:DescribeAutoScalingGroups"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
