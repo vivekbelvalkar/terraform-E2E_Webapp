ab188a7364edcc6b6cefa420165fdda5d7d18fa1
vivekbelvalkar <vvkbel@gmail.com>
2026-02-12 13:40:00 +0000

    Fix s3 access permissions for CodeBuild

--- modules/iam/codebuild_iam_role.tf
+++ modules/iam/codebuild_iam_role.tf
@@ -41,3 +41,37 @@ resource "aws_iam_role_policy" "codebuild_asg_policy" {
     ]
   })
 }
+
+resource "aws_iam_role_policy" "codebuild_logs_policy" {
+  role = aws_iam_role.codebuild.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "codebuild_s3_policy" {
+  role = aws_iam_role.codebuild.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["s3:ListBucket","s3:PutObject","s3:GetObject","s3:GetObjectVersion"]
+        Resource = "arn:aws:s3:::dev-ems-artifacts/*"
+      }
+    ]
+  })
+}
